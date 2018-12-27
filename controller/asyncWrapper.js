572bdd644eb756a982deb08cb2f38044158134fe
svetagimpelson <svetagimpelson@se.shenkar.ac.il>
2018-12-27 18:12:37 +0200

    update

--- controller/asyncWrapper.js
+++ controller/asyncWrapper.js
@@ -2,7 +2,7 @@
 
 module.exports = (fn) => (req, res, next) => {
     try { fn(req, res, next) } catch(err) {
-        console.error('some error occurred', err)
-        res.status(500).send(err.message)
+        console.error('some error occurred', err);
+        res.status(500).send(err.message);
     }
 }
--- controller/controller.js
+++ controller/controller.js
@@ -1,5 +1,5 @@
 const connection = require('../connection_info/connection'),
-      Group      = require('../model/groups')
+      Group      = require('../model/groups');
 
 
 //my cRUd functions
@@ -7,27 +7,33 @@ module.exports = {
     async getAllGroups(req, res, next) {
         const result = await Group.find({})
         if (result){
-            console.log(result)
-            res.json(result)
+            console.log(result);
+            res.json(result);
         } 
-        else res.status(404).send('not found')
+        else res.status(404).send('not found');
     },
     async setGroupScore(req, res, next) {
-        const {Name = null, Points = null} = req.body
-        const result = await Group.updateOne({Name},{Points})
+        const {Name = null, Points = null} = req.body;
+        const result = await Group.updateOne({Name},{Points});
         if (result) {
-            console.log(result)
-            res.json(result)
+            if (result.nModified !=0){
+                console.log('modified');
+                res.json({success:true});
+            }
+            else{
+                console.log('no changes made');
+                res.json({success:false});
+            }
         }
-         else res.status(404).send('not found')
+        else res.status(404).send('not found');
     },
     async groupsByScoreAndWin(req, res, next) {
-        const {points = null, wins = null} = req.body
-        const result = await Group.find({Points:{$gte:points},W:{$gte:wins}})
+        const {points = null, wins = null} = req.body;
+        const result = await Group.find({Points:{$gte:points},W:{$gte:wins}});
         if (result) {
-            console.log(result)
-            res.json(result)
+            console.log(result);
+            res.json(result);
         }
-        else res.status(404).send('not found')
+        else res.status(404).send('not found');
         }
 };
